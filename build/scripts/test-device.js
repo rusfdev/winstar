ef32424431006ded280fafbd69ad10d8b511a503
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2021-12-04 02:05:31 +0200

    eddited

--- build/scripts/test-device.js
+++ build/scripts/test-device.js
@@ -8,4 +8,4 @@ if (device.ios()) {
   document.documentElement.style.setProperty('--window-safe-height-property', `${window.innerHeight}px`);
 }
 
-document.documentElement.style.setProperty('--scrollbar-width-property', `${window.innerWidth - document.documentElement.clientWidth}px`);
+document.documentElement.style.setProperty('--scrollbar-width-property', `${window.innerWidth - document.body.clientWidth}px`);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,17 +8,43 @@ const breakpoints = {
 const $wrapper = document.querySelector('.wrapper');
 const $header = document.querySelector('.header');
 
+const animation_duration_1 = +getComputedStyle(document.documentElement).getPropertyValue('--animation-duration-1').replace(/[^\d.-]/g, '');
+const animation_duration_2 = +getComputedStyle(document.documentElement).getPropertyValue('--animation-duration-2').replace(/[^\d.-]/g, '');
+const animation_duration_3 = +getComputedStyle(document.documentElement).getPropertyValue('--animation-duration-3').replace(/[^\d.-]/g, '');
+
 import 'lazysizes';
+import {gsap} from "gsap";
+gsap.defaults({
+  ease: "power2.inOut",
+  duration: animation_duration_1 / 1000
+});
+import {disablePageScroll, enablePageScroll} from 'scroll-lock';
+
+//animations
+gsap.registerEffect({
+  name: "fadeIn",
+  effect: ($element, config) => {
+    return gsap.fromTo($element, {autoAlpha: 0}, {immediateRender: false, autoAlpha: 1, duration: config.duration || animation_duration_1 / 1000,
+      onStart: () => {
+        $element.forEach($this => {
+          $this.classList.add('d-block');
+        })
+      },
+      onReverseComplete: () => {
+        $element.forEach($this => {
+          gsap.set($this, {clearProps: "all"});
+          $this.classList.remove('d-block');
+        })
+      }
+    })
+  },
+  extendTimeline: true
+});
 
 document.addEventListener("DOMContentLoaded", function() {
   CustomInteractionEvents.init();
-
-  document.querySelectorAll('.header-search').forEach($this => {
-    new HeaderSearch($this).init();
-  })
-  document.querySelectorAll('.lang-toggle').forEach($this => {
-    new LangToggle($this).init();
-  })
+  HeaderSearch.init();
+  Modals.init();
 });
 
 const CustomInteractionEvents = Object.create({
@@ -94,27 +120,25 @@ const CustomInteractionEvents = Object.create({
   }
 })
 
-class HeaderSearch {
-  constructor($parent) {
-    this.$parent = $parent;
-  }
-
-  init() {
-    this._trigger_ = '.header-search__trigger';
-    this._form_ = '.header-search__form';
-    this.$trigger = this.$parent.querySelector(this._trigger_);
-    this.$form = this.$parent.querySelector(this._form_);
+const HeaderSearch = {
+  init: function() {
+    this._trigger_ = '.header-search-toggle';
+    this._parent_ = '.header-search';
+    this.$parent = document.querySelector(this._parent_);
+    this.$trigger = document.querySelector(this._trigger_);
 
     this.show = () => {
       this.state = true;
-      this.$form.classList.add('active');
       this.$trigger.classList.add('active');
+      this.$parent.classList.add('active');
+      document.documentElement.style.setProperty('--sticky-elements-safe-top', 'calc(var(--header-fixed-height) * 2)');
     }
 
     this.hide = () => {
       this.state = false;
-      this.$form.classList.remove('active');
       this.$trigger.classList.remove('active');
+      this.$parent.classList.remove('active');
+      document.documentElement.style.setProperty('--sticky-elements-safe-top', 'var(--header-fixed-height)');
     }
 
     this.$trigger.addEventListener('click', () => {
@@ -126,49 +150,65 @@ class HeaderSearch {
     })
 
     document.addEventListener('click', (event) => {
-      if (!event.target.closest(this._trigger_) && !event.target.closest(this._form_) && this.state) {
+      if (!event.target.closest(this._trigger_) && !event.target.closest(this._parent_) && this.state) {
         this.hide();
       }
     })
   }
 }
 
-class LangToggle {
-  constructor($parent) {
-    this.$parent = $parent;
-  }
+const Modals = {
+  init: function () {
 
-  init() {
-    this._trigger_ = '.lang-toggle__selected';
-    this._langs_ = '.lang-toggle__langs';
+    this.open = ($modal) => {
+      if($modal == this.$active || (this.animation && this.animation.isActive())) return;
 
-    this.$trigger = this.$parent.querySelector(this._trigger_);
-    this.$langs = this.$parent.querySelector(this._langs_);
+      let event = ()=> {
+        this.animation = gsap.timeline({paused: true}).fadeIn($modal);
+        
+        //mobile nav
+        if ($modal.classList.contains('mobile-nav')) {
+          const $inner = $modal.querySelector('.mobile-nav__container');
+          this.animation.add(
+            gsap.fromTo($inner, {x: -50}, {x: 0, ease:'power2.out'}), '<'
+          )
+        }
 
-    this.show = () => {
-      this.state = true;
-      this.$langs.classList.add('active');
-      this.$trigger.classList.add('active');
-    }
 
-    this.hide = () => {
-      this.state = false;
-      this.$langs.classList.remove('active');
-      this.$trigger.classList.remove('active');
-    }
+        this.animation.play().eventCallback('onStart', () => {
+          disablePageScroll();
+        });
 
-    this.$trigger.addEventListener('click', () => {
-      if (!this.state) {
-        this.show();
-      } else {
-        this.hide();
+        this.$active = $modal;
       }
-    })
+      
+      if(this.$active) this.close(event);
+      else event();
+    }
+
+    this.close = (callback) => {
+      if(!this.$active || (this.animation && this.animation.isActive())) return;
+
+      this.animation.reverse().eventCallback('onReverseComplete', () => {
+        enablePageScroll();
+        delete this.$active;
+        if(callback) callback();
+      })
+    }
 
     document.addEventListener('click', (event) => {
-      if (!event.target.closest(this._trigger_) && !event.target.closest(this._langs_) && this.state) {
-        this.hide();
+      let $open = event.target.closest('[data-action="Modal:open"]'),
+          $close = event.target.closest('[data-action="Modal:close"]');
+
+      if ($open) {
+        event.preventDefault();
+        let $target = document.querySelector(`${$open.getAttribute('href')}`);
+        if ($target) this.open($target);
+      } else if ($close) {
+        this.close();
       }
     })
+    
+    //this.open(document.querySelector('#mobile-nav'));
   }
 }
